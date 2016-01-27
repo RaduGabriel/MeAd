e9bb81b01e6852f80475543a1126d2b62e02727b
Gufa <focsa.mihaela@gmail.com>
2016-01-27 19:03:53 +0200

    search

--- project/src/MeAd/Controllers/SearchController.cs
+++ project/src/MeAd/Controllers/SearchController.cs
@@ -4,21 +4,44 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using VDS.RDF.Query;
+using Newtonsoft.Json;
 
 namespace MeAd.Raml
 {
     public partial class SearchController : ISearchController
     {
 
-		/// <summary>
-		/// Search by country name - Country
-		/// </summary>
-		/// <param name="countryName"></param>
+        /// <summary>
+        /// Search by country name - Country
+        /// </summary>
+        /// <param name="countryName"></param>
         public IActionResult Get(string countryName)
         {
+            Dictionary<string, string> diseases = new Dictionary<string, string>();
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("https://query.wikidata.org/sparql"), "https://query.wikidata.org");
+            SparqlResultSet results = endpoint.QueryWithResultSet(@"
+                                                                    PREFIX wd: <http://www.wikidata.org/entity/> 
+                                                                    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+                                                                    SELECT ?ID ?disease WHERE {
+                                                                    ?ID wdt:P699 ?doid .
+                                                                    ?ID rdfs:label ?disease filter (lang(?disease) = 'en').
 
+                                                                                filter regex(str(?disease), '" + countryName + "' )}"+
+                                                                   " limit 5");
+            foreach (SparqlResult result in results)
+            {
+                string id = result["ID"].ToString();
+                id = id.Remove(0, 31);
+                string name = result["disease"].ToString();
+                name = name.Remove(name.Length - 3);
+                diseases.Add(id, name);
+               
+                // richTextBox1.Text += wikidata_id + "\n";
+            }
             // TODO: implement Get - route: search/countries/{countryName}
-			return new ObjectResult(countryName);
+
+            return new ObjectResult(diseases);
         }
 
 		/// <summary>
